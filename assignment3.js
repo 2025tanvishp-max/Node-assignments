d60652392f2c035d54121baad0c7a45aac5bcdb6
Tanvish Poojari <2025.tanvishp@isu.ac.in>
2026-09-01 14:47:21 +0530

    Update assignment3.js

--- assignment3.js
+++ assignment3.js
@@ -6,9 +6,9 @@ const server = http.createServer((req, res) => {
 
         const student = {
             id: 101,
-            name: "John",
-            course: "BCA",
-            semester: 4,
+            name: "Tanvish",
+            course: "Btech",
+            semester: 3,
             city: "Mumbai"
         };
 
